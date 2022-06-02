b8f524c275d9490bff2f5016ae6090f05e0e2bce
Neelima1220 <neelimareddy1220@gmail.com>
2022-06-02 07:31:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-kg2mw3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-kg2mw3)
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+
+const accDara = [
+  { title: 'First Accordion', content: 'First Accordion content' },
+  { title: 'Second Accordion', content: 'Second Accordion content' },
+  {
+    title: 'Third Accordion',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
