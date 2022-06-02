d09eaa977659bdf5fc8156d043dcb492a483de03
Neelima1220 <neelimareddy1220@gmail.com>
2022-06-02 19:56:31 +0300

    1 file modified

--- src/compoents/accordian.js
+++ src/compoents/accordian.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
 const Accordion = ({ item, index }) => {
-  const [toggle, setToggle] = React.useState(false);
   const [selected, setSelected] = React.useState(null);
+  const [ind, setInd] = React.useState(null);
+
+  const handleAcc = (i) => {
+    if (selected === i) {
+      setSelected(null);
+      setInd(null);
+    } else {
+      setSelected(i);
+    }
+  };
 
   return (
     <>
       <h1
-        onClick={() => {
-          setToggle(!toggle);
-          setSelected(index);
-        }}
-        style={{ backgroundColor: selected === index ? 'red' : 'blue' }}
+        onClick={() => handleAcc(index)}
+        // style={{ backgroundColor: selected === index ? 'red' : 'blue' }}
       >
         {item.title}
       </h1>
-      <p>{item.content}</p>
+      <p style={{ display: selected === ind ? 'block' : 'none' }}>
+        {item.content}
+      </p>
     </>
   );
 };
