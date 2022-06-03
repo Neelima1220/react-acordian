8a4e225961a66fabcdfa6dd61fa94e4ceeb542dd
Neelima1220 <neelimareddy1220@gmail.com>
2022-06-03 08:42:54 +0300

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React from 'react';
+import { cloneDeep } from 'lodash';
 import Accordion from './compoents/accordian';
 import './style.css';
 
 const accData = [
-  { title: 'First Accordion', content: 'First Accordion content' },
-  { title: 'Second Accordion', content: 'Second Accordion content' },
+  {
+    title: 'First Accordion',
+    content: 'First Accordion content',
+    isOpen: false,
+  },
+  {
+    title: 'Second Accordion',
+    content: 'Second Accordion content',
+    isOpen: false,
+  },
   {
     title: 'Third Accordion',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    isOpen: false,
   },
 ];
 
 export default function App() {
   const [data, setData] = React.useState(accData);
+
+  const handleAcc = (i) => {
+    const tempAcc = cloneDeep(accData);
+    console.log(tempAcc, 'tempAcc');
+    tempAcc[i].isOpen = true;
+    setData(tempAcc);
+  };
   return data.map((item, index) => {
-    return <Accordion item={item} index={index} />;
+    return (
+      <Accordion
+        item={item}
+        index={index}
+        onClick={() => handleAcc(index)}
+        isOpen={item.isOpen}
+      />
+    );
   });
 }
--- src/compoents/accordian.js
+++ src/compoents/accordian.js
@@ -1,29 +1,12 @@
 import React from 'react';
 
-const Accordion = ({ item, index }) => {
+const Accordion = ({ item, index, isOpen, onClick }) => {
   const [selected, setSelected] = React.useState(null);
-  const [ind, setInd] = React.useState(null);
-
-  const handleAcc = (i) => {
-    if (selected === i) {
-      setSelected(null);
-      setInd(null);
-    } else {
-      setSelected(i);
-    }
-  };
 
   return (
     <>
-      <h1
-        onClick={() => handleAcc(index)}
-        // style={{ backgroundColor: selected === index ? 'red' : 'blue' }}
-      >
-        {item.title}
-      </h1>
-      <p style={{ display: selected === ind ? 'block' : 'none' }}>
-        {item.content}
-      </p>
+      <h1 onClick={onClick}>{item.title}</h1>
+      <p style={{ display: isOpen ? 'block' : 'none' }}>{item.content}</p>
     </>
   );
 };
